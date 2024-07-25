6740f456f165c98ac16ab6b4558f67145c774343
MrOrz <johnsonliang7@gmail.com>
2024-07-26 00:44:48 +0800

    feat(db): add virtual table for fts

---
+++ db/migrations/0002_create_fts_table.sql
@@ -0,0 +1,8 @@
+-- Migration number: 0002 	 2024-07-25T16:00:25.449Z
+
+CREATE VIRTUAL TABLE IF NOT EXISTS "ScamSiteRecordFTS" USING fts5(
+  name,
+  url,
+  content='ScamSiteRecord',
+  tokenize='trigram'
+);
