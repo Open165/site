891820284bd658167ed1ee29fd4e7c96a1ba7a95
MrOrz <johnsonliang7@gmail.com>
2024-07-29 00:32:07 +0800

    docs(db): add comments in migration, which adds comment to schema as well

--- db/migrations/0001_create_scam_site_record_table.sql
+++ db/migrations/0001_create_scam_site_record_table.sql
@@ -1,14 +1,15 @@
 -- CreateTable
--- Based on 165反詐騙諮詢專線_假投資(博弈)網站
--- URL: https://data.gov.tw/dataset/160055
-CREATE TABLE "ScamSiteRecord" (
+CREATE TABLE "ScamSiteRecord"
+    -- Based on 165反詐騙諮詢專線_假投資(博弈)網站
+    -- URL: https://data.gov.tw/dataset/160055
+(
     "name" TEXT NOT NULL,
     "url" TEXT NOT NULL,
-    "count" INTEGER NOT NULL,
-    "startDate" TEXT NOT NULL,
-    "endDate" TEXT NOT NULL,
+    "count" INTEGER NOT NULL, -- number of reports
+    "startDate" TEXT NOT NULL, -- number of reports calculation start date in YYYY/MM/DD
+    "endDate" TEXT NOT NULL, -- number of reports calculation end date in YYYY/MM/DD
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "host" TEXT NOT NULL
+    "host" TEXT NOT NULL -- Extracted from URL
 );
 
 -- CreateIndex
--- db/schema.sql
+++ db/schema.sql
@@ -13,14 +13,17 @@ CREATE TABLE d1_migrations(
 
 CREATE TABLE sqlite_sequence(name,seq)
 
-CREATE TABLE "ScamSiteRecord" (
+CREATE TABLE "ScamSiteRecord"
+    -- Based on 165反詐騙諮詢專線_假投資(博弈)網站
+    -- URL: https://data.gov.tw/dataset/160055
+(
     "name" TEXT NOT NULL,
     "url" TEXT NOT NULL,
-    "count" INTEGER NOT NULL,
-    "startDate" TEXT NOT NULL,
-    "endDate" TEXT NOT NULL,
+    "count" INTEGER NOT NULL, -- number of reports
+    "startDate" TEXT NOT NULL, -- number of reports calculation start date in YYYY/MM/DD
+    "endDate" TEXT NOT NULL, -- number of reports calculation end date in YYYY/MM/DD
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "host" TEXT NOT NULL
+    "host" TEXT NOT NULL -- Extracted from URL
 )
 
 CREATE INDEX "ScamSiteRecord_host_idx" ON "ScamSiteRecord"(lower("host"))
