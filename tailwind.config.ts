f6493c46862d248561198842e4c0cb14366dc3a1
MrOrz <johnsonliang7@gmail.com>
2024-08-11 20:09:08 +0800

    fix: remove unused tailwind rule and fix extension logic

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,22 +3,23 @@ import { nextui } from '@nextui-org/react';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    fontFamily: {
-      sans: ['Noto Sans TC', 'sans-serif'],
-      serif: ['Noto Serif TC', 'serif'],
-    },
-    fontSize: {
-      // From local styles in Figma
-      body: ['16px', '24px'],
-      body2: ['14px', '20px'],
-      heading: ['18px', '28px'],
-      display: ['57px', '64px'],
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans TC', 'sans-serif'],
+        serif: ['Noto Serif TC', 'serif'],
+      },
+      fontSize: {
+        // From local styles in Figma
+        body: ['16px', '24px'],
+        body2: ['14px', '20px'],
+        heading: ['18px', '28px'],
+        display: ['57px', '64px'],
+      },
     },
   },
   darkMode: 'class',
