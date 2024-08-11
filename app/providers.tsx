fff3d4cef107f32963dd1bfda10c0ad6ad8b239a
MrOrz <johnsonliang7@gmail.com>
2024-08-11 21:22:50 +0800

    docs(providers): add doc

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,6 +4,10 @@ import type React from 'react';
 import { NextUIProvider } from '@nextui-org/react';
 import { useRouter } from 'next/navigation';
 
+/**
+ * NextUI with routing setup
+ * @ref https://nextui.org/docs/guide/routing#nextjs
+ */
 export default function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
