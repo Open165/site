241043e5f1d4c755179f7148d34a03cdb5bd9a4b
MrOrz <johnsonliang7@gmail.com>
2024-09-13 00:49:22 +0800

    feat: add summary text for site name

--- app/name/util.tsx
+++ app/name/util.tsx
@@ -31,3 +31,22 @@ export const getRecords = cache(async (name: string) => {
 
   return Promise.all([directHitPromise, searchSimilar(name)]);
 });
+
+/** Get summary text for a site name */
+export function getSummary(name: string, directHits: Record[]) {
+  if (directHits.length === 0) {
+    return `內政部警政署公布的詐騙網站中，目前沒有「${name}」。`;
+  }
+
+  const latest = directHits[0];
+  const countSum = directHits.reduce((sum, { count }) => sum + count, 0);
+
+  return (
+    <>
+      近期詐團{' '}
+      <mark className="highlighter">使用「{latest.name}」名義行騙</mark>。
+      內政部警政署已收到 {countSum} 次民眾檢舉以「{latest.name}」為名的網站，
+      最近 {latest.count} 次落在 {latest.endDate} 那週，請務必小心！
+    </>
+  );
+}
