4546983b6f29a9c20d38ec8a232c745e67fb3343
MrOrz <johnsonliang7@gmail.com>
2024-09-18 01:47:42 +0800

    feat(db): add ScamSiteAnnouncement table

--- db/schema.sql
+++ db/schema.sql
@@ -44,3 +44,15 @@ CREATE TABLE 'ScamSiteRecordFTS_idx'(segid, term, pgno, PRIMARY KEY(segid, term)
 CREATE TABLE 'ScamSiteRecordFTS_docsize'(id INTEGER PRIMARY KEY, sz BLOB)
 
 CREATE TABLE 'ScamSiteRecordFTS_config'(k PRIMARY KEY, v) WITHOUT ROWID
+
+CREATE TABLE "ScamSiteAnnouncement"
+    -- 165反詐騙官網「民眾通報假投資(博弈)詐騙網站」系列公告
+(
+    "id" INTEGER NOT NULL PRIMARY KEY,
+    "endDate" TEXT NOT NULL, -- Announcement range end in "YYYY/MM/DD", maps to "endDate" in "ScamSiteRecord"
+    "publishDate" TEXT NOT NULL -- Announcement publish date in "YYYY-MM-DDThh:mm:ss+08:00"
+)
+
+CREATE INDEX "ScamSiteAnnouncement_endDate_idx" ON "ScamSiteAnnouncement"("endDate")
+
+CREATE INDEX "ScamSiteRecord_endDate_idx" ON "ScamSiteRecord"("endDate")
